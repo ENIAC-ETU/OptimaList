c7125f77be4d37e932206231c4c193852f5f87cb
Furkan YILMAZ <f.yilmaz@etu.edu.tr>
2019-01-12 17:22:20 +0300

    add create, delete and update shopping lists feature. fixes #1

--- app/src/main/java/com/eniac/optimalist/MainActivity.java
+++ app/src/main/java/com/eniac/optimalist/MainActivity.java
@@ -1,12 +1,18 @@
 package com.eniac.optimalist;
 
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationChannel;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,31 +24,52 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.app.NotificationManager;
 import android.support.v4.app.NotificationCompat;
-import android.widget.Button;
-import android.app.NotificationChannel;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.eniac.optimalist.database.DBHelper;
+import com.eniac.optimalist.database.model.ShoppingList;
+import com.eniac.optimalist.utils.DividerItemDecoration;
+import com.eniac.optimalist.utils.RecyclerTouchListener;
+import com.eniac.optimalist.view.ShoppingListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     public static final String CHANNEL_1_ID = "Channel 1";
     NotificationManagerCompat notificationManager;
+    private DBHelper db;
+    private ShoppingListAdapter shoppingListAdapter;
+    private List<ShoppingList> shoppingLists = new ArrayList<>();
+    private DrawerLayout drawer;
+    private RecyclerView recyclerView;
+    private TextView noShoppingListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DBHelper(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        recyclerView = findViewById(R.id.recycler_view);
+        noShoppingListView = findViewById(R.id.empty_shopping_lists_view);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showShoppingListDialog(false, null, -1);
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -51,12 +78,38 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-
         addNotification();
          notificationManager = NotificationManagerCompat.from(this);
 
         sendOnChannel1(null);
 
+        shoppingLists.addAll(db.getAllShoppingLists());
+        shoppingListAdapter = new ShoppingListAdapter(this, shoppingLists);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL, 16));
+        recyclerView.setAdapter(shoppingListAdapter);
+
+        toggleEmptyShoppingLists();
+
+        /*
+         * On long press on RecyclerView item, open alert dialog
+         * with options to choose
+         * Edit and Delete
+         * */
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(this,
+                recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, final int position) {
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                showActionsDialog(position);
+            }
+        }));
     }
 
     @Override
@@ -143,5 +196,156 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    /**
+     * Inserting new shopping list in db
+     * and refreshing the list
+     */
+    private void createShoppingList(String title) {
+        // inserting shopping list in db and getting
+        // newly inserted shopping list id
+        long id = db.insertShoppingList(title);
+
+        // get the newly inserted shopping list from db
+        ShoppingList l = db.getShoppingList(id);
+
+        if (l != null) {
+            // adding new shopping list to array list at 0 position
+            shoppingLists.add(0, l);
+
+            // refreshing the list
+            shoppingListAdapter.notifyDataSetChanged();
+
+            toggleEmptyShoppingLists();
+        }
+    }
+
+    /**
+     * Updating shopping list in db and updating
+     * item in the list by its position
+     */
+    private void updateShoppingList(String title, int position) {
+        ShoppingList l = shoppingLists.get(position);
+        // updating shopping list title
+        l.setTitle(title);
+
+        // updating note in db
+        db.updateShoppingList(l);
+
+        // refreshing the list
+        shoppingLists.set(position, l);
+        shoppingListAdapter.notifyItemChanged(position);
+
+        toggleEmptyShoppingLists();
+    }
+
+    /**
+     * Deleting shopping list from SQLite and removing the
+     * item from the list by its position
+     */
+    private void deleteShoppingList(int position) {
+        // deleting the shopping list from db
+        db.deleteShoppingList(shoppingLists.get(position));
+
+        // removing the shopping list from the list
+        shoppingLists.remove(position);
+        shoppingListAdapter.notifyItemRemoved(position);
+
+        toggleEmptyShoppingLists();
+    }
+
+    /**
+     * Opens dialog with Edit - Delete options
+     * Edit - 0
+     * Delete - 0
+     */
+    private void showActionsDialog(final int position) {
+        CharSequence colors[] = new CharSequence[]{"Düzenle", "Sil"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Bir seçenek seçiniz");
+        builder.setItems(colors, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0) {
+                    showShoppingListDialog(true, shoppingLists.get(position), position);
+                } else {
+                    deleteShoppingList(position);
+                }
+            }
+        });
+        builder.show();
+    }
+
+    /**
+     * Shows alert dialog with EditText options to enter / edit
+     * a shopping list.
+     * when shouldUpdate=true, it automatically displays old shopping list and changes the
+     * button text to UPDATE
+     */
+    private void showShoppingListDialog(final boolean shouldUpdate, final ShoppingList shoppingList, final int position) {
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflaterAndroid.inflate(R.layout.shopping_list_dialog, null);
+
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilderUserInput.setView(view);
+
+        final EditText inputShoppingList = view.findViewById(R.id.shopping_list);
+        TextView dialogTitle = view.findViewById(R.id.dialog_title);
+        dialogTitle.setText(!shouldUpdate ? getString(R.string.lbl_new_shopping_list_title) : getString(R.string.lbl_edit_shopping_list_title));
+
+        if (shouldUpdate && shoppingList != null) {
+            inputShoppingList.setText(shoppingList.getTitle());
+        }
+        alertDialogBuilderUserInput
+                .setCancelable(false)
+                .setPositiveButton(shouldUpdate ? "güncelle" : "kaydet", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogBox, int id) {
+
+                    }
+                })
+                .setNegativeButton("iptal",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                dialogBox.cancel();
+                            }
+                        });
+
+        final AlertDialog alertDialog = alertDialogBuilderUserInput.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show toast message when no text is entered
+                if (TextUtils.isEmpty(inputShoppingList.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Alışveriş listesi adını giriniz!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    alertDialog.dismiss();
+                }
+
+                // check if user updating note
+                if (shouldUpdate && shoppingList != null) {
+                    // update note by it's id
+                    updateShoppingList(inputShoppingList.getText().toString(), position);
+                } else {
+                    // create new note
+                    createShoppingList(inputShoppingList.getText().toString());
+                }
+            }
+        });
+    }
+
+    /**
+     * Toggling list and empty shopping lists view
+     */
+    private void toggleEmptyShoppingLists() {
+        if (db.getShoppingListsCount() > 0) {
+            noShoppingListView.setVisibility(View.GONE);
+        } else {
+            noShoppingListView.setVisibility(View.VISIBLE);
+        }
     }
 }
---
+++ app/src/main/java/com/eniac/optimalist/database/DBHelper.java
@@ -0,0 +1,104 @@
+package com.eniac.optimalist.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.eniac.optimalist.database.model.ShoppingList;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "optimalist.db";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(ShoppingList.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + ShoppingList.TABLE_NAME);
+        onCreate(db);
+    }
+
+    public long insertShoppingList(String title) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ShoppingList.COLUMN_TITLE, title);
+        return db.insert(ShoppingList.TABLE_NAME, null, contentValues);
+    }
+
+    public ShoppingList getShoppingList(long id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        ShoppingList shoppingList = null;
+
+        try (Cursor cursor = db.query(ShoppingList.TABLE_NAME, null, ShoppingList.COLUMN_ID + " = " + id, null, null, null, null)){
+            if (cursor.moveToFirst()) {
+                shoppingList = new ShoppingList(
+                        cursor.getInt(cursor.getColumnIndex(ShoppingList.COLUMN_ID)),
+                        cursor.getString(cursor.getColumnIndex(ShoppingList.COLUMN_TITLE)),
+                        cursor.getString(cursor.getColumnIndex(ShoppingList.COLUMN_CREATED_AT))
+                );
+            }
+        }
+
+        return shoppingList;
+    }
+
+    public int updateShoppingList(ShoppingList shoppingList) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(shoppingList.COLUMN_TITLE, shoppingList.getTitle());
+
+        // updating row
+        return db.update(ShoppingList.TABLE_NAME, values, ShoppingList.COLUMN_ID + " = " + shoppingList.getId(), null);
+    }
+
+    public boolean deleteShoppingList(ShoppingList shoppingList) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(ShoppingList.TABLE_NAME, ShoppingList.COLUMN_ID + " = " + shoppingList.getId(), null) > 0;
+    }
+
+    public List<ShoppingList> getAllShoppingLists() {
+        List<ShoppingList> shoppingLists = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        try (Cursor cursor = db.query(ShoppingList.TABLE_NAME, null, null, null, null, null, ShoppingList.COLUMN_CREATED_AT + " desc")){
+            if (cursor.moveToFirst()) {
+                do {
+                    shoppingLists.add(new ShoppingList(
+                                    cursor.getInt(cursor.getColumnIndex(ShoppingList.COLUMN_ID)),
+                                    cursor.getString(cursor.getColumnIndex(ShoppingList.COLUMN_TITLE)),
+                                    cursor.getString(cursor.getColumnIndex(ShoppingList.COLUMN_CREATED_AT))
+                            )
+                    );
+                } while (cursor.moveToNext());
+            }
+        }
+
+        return shoppingLists;
+    }
+
+    public int getShoppingListsCount() {
+        String countQuery = "select * from " + ShoppingList.TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+
+        int count = cursor.getCount();
+        cursor.close();
+
+        // return count
+        return count;
+    }
+}
